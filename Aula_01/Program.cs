c81a8a53bcaee9c2c4b9b11e9814ba1a4dce6356
danilo borges <danilosilva2017@hotmail.com>
2023-01-08 06:50:51 -0300

    Entendo Abstracao e Herenca

--- Aula_01/Program.cs
+++ Aula_01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Payments {
+    class Program {
+        static void Main(string[] args){
+
+            
+
+        }
+    }
+
+    class Pagamento {
+        DateTime Vencimento {get; set;}
+        public int MyProperty { get; set; }     
+    }
+}

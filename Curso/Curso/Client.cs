7935594019eaebacf9afcb7c407fe486fbc03fac
danilo borges <danilosilva2017@hotmail.com>
2023-01-14 05:22:26 -0300

    Implementando IEquatable em uma classe Client

---
+++ Curso/Curso/Client.cs
@@ -0,0 +1,23 @@
+﻿namespace Curso
+{
+    internal class Client : IEquatable<Client>
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public Client(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public bool Equals(Client? other)
+        {
+            if (this.Id == other.Id && this.Name == other.Name && this.Age == other.Age)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Curso/Curso/Program.cs
@@ -0,0 +1,7 @@
+﻿using Curso;
+
+var client = new Client(10, "Danilo", 30);
+
+var client2 = new Client(10, "João", 30);
+
+Console.WriteLine(client.Equals(client2));

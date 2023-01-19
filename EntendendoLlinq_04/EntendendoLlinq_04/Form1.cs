8bb2c5d7f0fb4600508991fcb529a905f169bd70
danilo borges <danilosilva2017@hotmail.com>
2023-01-19 06:44:38 -0300

    Adicionado o Metodo LinqFiltrar que faz a busca na lista de Produtos e retorna produtos com ID menor que 15

--- EntendendoLlinq_04/EntendendoLlinq_04/Form1.cs
+++ EntendendoLlinq_04/EntendendoLlinq_04/Form1.cs
@@ -9,7 +9,20 @@ namespace EntendendoLlinq_04
 
         private void btnOk_Click(object sender, EventArgs e)
         {
+            List<Produto> listaProdutos = new Produto().GetAll();
+            LinqFiltrar(listaProdutos);
+        }
+        private void LinqFiltrar (List<Produto> listaProdutos)
+        {
+            var listaProdutosFiltrada =
+                from produto in listaProdutos
+                where produto.Id < 15
+                select produto;
 
+            foreach (var produto in listaProdutosFiltrada)
+            {
+                MessageBox.Show($"ID {produto.Id} - {produto.Nome}");
+            }
         }
     }
 }
